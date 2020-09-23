240b58762e089993baa7b7908d2c6906c7b2e49f
YannickRosheuvel <62062906+YannickRosheuvel@users.noreply.github.com>
2020-09-23 10:04:00 +0200

    Unit test project added

---
+++ AchmeaProject/AchmeaTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AchmeaTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
